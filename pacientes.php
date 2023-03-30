ec0637fc840ff34df70868bffd5d22a884f45895
szambrano21 <szambrano21@ilg.cat>
2023-03-30 17:40:46 +0200

    page pacientes

--- pacientes.php
+++ pacientes.php
@@ -29,50 +29,48 @@ include_once('connexiobbddsanitat.php');
     <?php
 
     /* PAGINADOR */
-    /*
-
-    $sql_registro = mysqli_query($conexion, "SELECT COUNT(*) as total_registro FROM tdades
-    WHERE nom");
+    $sql_registro = mysqli_query($conexion, "SELECT COUNT(*) as total_registro FROM tdades");
 
     $resultado_registro = mysqli_fetch_assoc($sql_registro);
     $total_registro = $resultado_registro['total_registro'];
-
-    $por_pagina = 5;
-
+  
+    $por_pagina = 6;
+  
     if(empty($_GET['pagina'])){
-    $pagina = 1;
+      $pagina = 1;
     }else{
-    $pagina = $_GET['pagina'];
+      $pagina = $_GET['pagina'];
     }
-
+  
     $desde = ($pagina -1) * $por_pagina;
     $total_paginas = ceil($total_registro / $por_pagina);
-    */
-
-    $sql = mysqli_query($conexion, "SELECT nom, nHc FROM tdades 
+    $sql = mysqli_query($conexion, "SELECT * FROM tdades ORDER BY nom ASC LIMIT $desde,$por_pagina
     ");
 
+    /*$sql = mysqli_query($conexion, "SELECT nom, nHc FROM tdades 
+    ");*/
+
     $resultado= mysqli_num_rows($sql);
-    echo $resultado;
+
     if($resultado > 0){
 
         ?>
         <div class="container_general">
         <div class="espacio_arriba"></div>
-            <form action="buscar_farmacos.php" class="form_container"  method="get" name="formu">
-
-                <div class="field" id="searchform">
-                <input class="inputs" id="busqueda" name="busqueda" type="text" placeholder="Coloca un fármaco" value="<?php echo $busqueda; ?>"/>
+        <form action="proba_buscador_paciente.php" class="form_container"  method="get" name="formu">
+            <div class="field" id="searchform">
+                <input class="inputs" id="busqueda" name="busqueda" type="text" placeholder="Coloca un nombre.." />
                 <button type="submit" value="buscar"><img class="iconSearch" src="https://img.icons8.com/material-outlined/256/search.png"></button>
-                </div>
-            </form>
-            <br>
+            </div>
+        </form>
+        <br>
         <?php
         echo '    
         <div class="container_paciente">';
-    while ($row = mysqli_fetch_assoc($sql)) {
-        $nom = $row['nom'];
-        $nHc = $row['nHc'];
+    
+        while ($row = mysqli_fetch_assoc($sql)) {
+            $nom = $row['nom'];
+            $nHc = $row['nHc'];
         
     ?>
 
@@ -93,23 +91,28 @@ include_once('connexiobbddsanitat.php');
 
     ?>
 
-    <div class="pagination"></div>
+    <div class="pagination">
     <?php
-    /*if ($pagina > 1) {
-        echo "<li><a href='?pagina=".($pagina-1)."&busqueda=".$busqueda."'>Anterior</a></li>";
-    }
-
-    for ($i = 1; $i <= $total_paginas; $i++) {
-        if ($i == $pagina) {
-        echo "<li><a class='pagina-actual'>$i</a></li>";
-        } else {
-        echo "<li><a href='?pagina=$i.&busqueda=".$busqueda."'>$i</a></li>";
+        if ($pagina > 1) {
+            echo "<li><a href='?pagina=".($pagina-1)."'>Anterior</a></li>";
         }
-    }
 
-    if ($pagina < $total_paginas) {
-        echo "<li><a href='?pagina=".($pagina+1)."&busqueda=".$busqueda."'>Siguiente</a></li>";
-    }*/
+        for ($i = 1; $i <= $total_paginas; $i++) {
+            if ($i == $pagina) {
+            echo "<li><a class='pagina-actual'>$i</a></li>";
+            } else {
+            echo "<li><a href='?pagina=$i'>$i</a></li>";
+            }
+        }
+
+        if ($pagina < $total_paginas) {
+            echo "<li><a href='?pagina=".($pagina+1)."'>Siguiente</a></li>";
+        }
+    ?>
+    </div>
+
+    <?php
+        mysqli_close($conexion); //cierra la BBDD
     ?>
 
 </div>
