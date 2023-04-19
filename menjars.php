b925a21ce5a5e2d2036f3ec4f3f88d6c80c68eff
Sara <szambrano21@ilg.cat>
2023-04-20 01:58:11 +0200

    forms

--- menjars.php
+++ menjars.php
@@ -28,41 +28,96 @@
     ?>
     <div class="hero">
       <form>    
-            <h2>Necessitats de menjar i beguda</h2>
-            <hr>
-            <br>
-			    <div class="row">
-					<div class="input-group">
-                            <input type="text" id="pes" name="pes" required />
-                            <label for="pes"><i class="fa-solid fa-weight-scale"></i> Pes</label>
-						</div>
-						<div class="input-group">
-								<input type="text" id="talla" required>
-								<label for="talla"><i class="fa-solid fa-ruler-vertical"></i> talla</label>
-						</div>
-                        <div class="input-group">
-								<input type="text" id="talla" required>
-								<label for="talla"><i class="fa-solid fa-ruler-vertical"></i> talla</label>
-				        </div>
-				</div>
-				<div class="input-group">
-						<input type="text" id="dietaHabitual" required>
-						<label for="dietaHabitual"><i class="fa-solid fa-utensils"></i> Dieta Habitual</label>
-				</div>
-				<div class="input-group">
-						<input type="text" id="alimentsNoGrassos" required>
-						<label for="alimentsNoGrassos"><i class="fa-solid fa-bowl-food"></i> Aliments no grats</label>
-				</div>
-				<div class="input-group">
-						<textarea id="protesisDental" rows="8" required></textarea>
-						<label for="protesisDental"><i class="fas fa-comments"></i> protesis Dental</label>
-				</div>
-                <div class="input-group">
-						<textarea id="message" rows="8" required></textarea>
-						<label for="message"><i class="fas fa-comments"></i> Mensaje</label>
-				</div>
+        <h2>Necessitats de menjar i beguda</h2>
+        <hr>
+        <br>
+		<div class="row">
+		    <div class="input-group">
+                <input type="text" id="pes" name="pes" required />
+                <label class="labelForm" for="pes"><i class="fa-solid fa-weight-scale"></i> Pes</label>
+			</div>
+			<div class="input-group">
+                <input type="text" id="talla" required>
+                <label class="labelForm" for="talla"><i class="fa-solid fa-ruler-vertical"></i> talla</label>
+			</div>
+            <div class="input-group">
+                <input type="text" id="talla" required>
+                <label class="labelForm" for="talla"><i class="fa-solid fa-ruler-vertical"></i> talla</label>
+            </div>
+		</div>
+		<div class="input-group">
+			<input type="text" id="dietaHabitual" required>
+			<label class="labelForm" for="dietaHabitual"><i class="fa-solid fa-utensils"></i> Dieta Habitual</label>
+		</div>
+        <div class="input-group">
+            <input type="text" id="alimentsNoGrassos" required>
+            <label class="labelForm" for="alimentsNoGrassos"><i class="fa-solid fa-bowl-food"></i> Aliments no grats</label>
+        </div>
+        <div class="row">
+            <div>
+                <label>protesis Dental</label>
+                <div class="input-group-check">
+                    <input type="checkbox" class="checkbox" id="protesisDental" name ="protesisDental" value="protesisDental"  required>SI
+                    <input type="checkbox" class="checkbox" id="protesisDental" name ="protesisDental" value="protesisDental"  required>NO
+                </div>
+            </div>
+            <div>
+                <label>dificultat</label>
+                <div class="input-group-check">
+                    <input type="checkbox" name="dificultat" value="Masticacion"> Masticacion
+                    <input type="checkbox" name="dificultat" value="Deglucio"> Deglucio
+                    <input type="checkbox" name="dificultat" value="Digestio"> Digestio
+                </div>
+            </div>
+        </div>
 
-				<button type="submit">ENVIAR <i class="fas fa-paper-plane"></i></button>
+        
+        <!--
+        <div class="input-group">
+            <label>Necessitat d'ajut:</label>
+            <div class="input-group-check">
+                <input type="checkbox" name="protesisDental" value="gusta"> Total
+                <input type="checkbox" name="protesisDental" value="gusta"> Parcial
+                <input type="checkbox" name="protesisDental" value="gusta"> Independent
+            </div>
+            <label>Signes de plec</label>
+            <div class="input-group-check">
+                <input type="checkbox" id="protesisDental" name="protesisDental" value="si"> Si
+                <input type="checkbox" id="protesisDental"  name="protesisDental" value="no"> No
+            </div>
+        <div>
+
+    -->
+        <div class="row">
+            <div>
+                <label>Necessitat d'ajut</label>
+                <div class="input-group-check">
+                    <input type="checkbox" name="protesisDental" value="gusta"> Total
+                    <input type="checkbox" name="protesisDental" value="gusta"> Parcial
+                    <input type="checkbox" name="protesisDental" value="gusta"> Independent
+                </div>
+            </div>
+            <div>
+                <label>Signes de plec</label>
+                <div class="input-group-check">
+                    <input type="checkbox" name="protesisDental" value="si"> Si
+                    <input type="checkbox" name="protesisDental" value="no"> No
+                </div>
+            </div>
+        </div>
+        <div>
+            <label>Inapetència i/o anorèxia</label>
+            <div class="input-group-check">
+                <input type="checkbox" name="inapetenciaAnorexia" value="si"> Si
+                <input type="checkbox" name="inapetenciaAnorexia" value="no"> No
+            </div>
+        </div>
+        <div class="input-group">
+            <textarea id="observacions" rows="8" required></textarea>
+            <label class="labelForm" for="observacions"><i class="fas fa-comments"></i> Observacions</label>
+        </div>
+
+		<button class="buttonSubmit" type="submit">ENVIAR <i class="fas fa-paper-plane"></i></button>
       </form>
     </div>
 
