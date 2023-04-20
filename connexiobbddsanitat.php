8ee441bd47ec2503e8da1a8af2eda100332f9280
izambrano19 <izambrano19@ilg.cat>
2023-04-20 13:16:24 +0200

    subido

--- connexiobbddsanitat.php
+++ connexiobbddsanitat.php
@@ -8,3 +8,15 @@ define('NUM_ITEMS_BY_PAGE', 6);
 if (!$conexion) die("No ha podido realizarse la conexión".mysqli_connect_error());
 else 
 ?>
+
+
+<!-- <?php
+// $host = "projectedawilg3.cat";
+// $database = "zb5b2zsl_bbdd_sanitat";
+// $user = "zb5b2zsl_proyecto3";
+// $password = "proyecto3sanitat";
+// $conexion = mysqli_connect($host, $user, $password,$database);
+// define('NUM_ITEMS_BY_PAGE', 6);
+// if (!$conexion) die("No ha podido realizarse la conexión".mysqli_connect_error());
+// else 
+?> -->
