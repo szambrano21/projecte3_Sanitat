2fe766551553aea2a76e7d9bb99b27eb8f194837
izambrano19 <izambrano19@ilg.cat>
2023-05-04 18:17:14 +0200

    avances

--- nav.php
+++ nav.php
@@ -46,7 +46,7 @@
         </div>
                     <ul class="menu_nav">
 
-                        <li><span href="inicial.php">LOGOTIPO</span></li>
+                        <li><span class="logotipo" href="inicial.php">LOGOTIPO</span></li>
                         <li><a class="active" href="inicial.php">DASHBOARD</a></li>
                         <li><a href="listadoUsuario.php">Usuarios</a></li>
                         <li><a href="pacientes.php">Pacientes</a></li>
