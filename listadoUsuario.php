ea25c10f0cf0423a24ad8c1988a60d1326fb033f
izambrano19 <izambrano19@ilg.cat>
2023-05-04 16:00:55 +0200

    avances

--- listadoUsuario.php
+++ listadoUsuario.php
@@ -3,64 +3,66 @@ include_once('connexiobbddsanitat.php');
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-<?php include_once("scripts.php"); ?>
-<?php
-include_once('connexiobbddsanitat.php');
 
-?>
+<head>
+  <?php include_once("scripts.php"); ?>
+  <?php
+  include_once('connexiobbddsanitat.php');
+
+  ?>
 
 </head>
+
 <body>
-<?php   
-    include_once("header.php");
+  <?php
+  include_once("header.php");
 
-    if($_SESSION['tipo'] != 'admin'){
-      header("location: inicial.php");
-    }
-     ?>
+  if ($_SESSION['tipo'] != 'admin') {
+    header("location: inicial.php");
+  }
+  ?>
 
-    <!-- container_general - no tocar -->
-    <div class="container_general">
+  <!-- container_general - no tocar -->
+  <div class="container_general">
     <div class="second_container">
 
 
-    <!-- BARRA DE NAVEGACION -->
+      <!-- BARRA DE NAVEGACION -->
 
 
 
-    <!-- ESPACIO -->
+      <!-- ESPACIO -->
 
 
-    
-<?php
 
-/* PAGINADOR */
+      <?php
 
-$sql_registro = mysqli_query($conexion, "SELECT COUNT(*) as total_registro FROM tusuaris");
+      /* PAGINADOR */
 
-$resultado_registro = mysqli_fetch_assoc($sql_registro);
-$total_registro = $resultado_registro['total_registro'];
+      $sql_registro = mysqli_query($conexion, "SELECT COUNT(*) as total_registro FROM tusuaris");
 
-$por_pagina = 5;
+      $resultado_registro = mysqli_fetch_assoc($sql_registro);
+      $total_registro = $resultado_registro['total_registro'];
 
-if(empty($_GET['pagina'])){
-  $pagina = 1;
-}else{
-  $pagina = $_GET['pagina'];
-}
+      $por_pagina = 5;
 
-$desde = ($pagina -1) * $por_pagina;
-$total_paginas = ceil($total_registro / $por_pagina);
+      if (empty($_GET['pagina'])) {
+        $pagina = 1;
+      } else {
+        $pagina = $_GET['pagina'];
+      }
 
-$sql = mysqli_query($conexion, "SELECT * FROM tusuaris ORDER BY nomUsuari ASC LIMIT $desde,$por_pagina
+      $desde = ($pagina - 1) * $por_pagina;
+      $total_paginas = ceil($total_registro / $por_pagina);
+
+      $sql = mysqli_query($conexion, "SELECT * FROM tusuaris ORDER BY nomUsuari ASC LIMIT $desde,$por_pagina
 ");
 
-$resultado = mysqli_num_rows($sql);
+      $resultado = mysqli_num_rows($sql);
 
-if($resultado > 0){
+      if ($resultado > 0) {
 
-  echo "
+        echo "
 
   <table class='tablas_usuarios'>
     <thead>
@@ -74,17 +76,17 @@ if($resultado > 0){
     </thead>
 
   ";
-  
-  while ($row = mysqli_fetch_assoc($sql)) {
 
-    $nomUsuari = $row["nomUsuari"];
-    $cognomUsuari = $row["cognomUsuari"];
-    $dni = $row["DNI"];
-    $telefono = $row["telefono"];
+        while ($row = mysqli_fetch_assoc($sql)) {
+
+          $nomUsuari = $row["nomUsuari"];
+          $cognomUsuari = $row["cognomUsuari"];
+          $dni = $row["DNI"];
+          $telefono = $row["telefono"];
 
 
-    
-    echo"
+
+          echo "
 
     <tr>
         <td titulo='NOMBRE:'>$nomUsuari</td>
@@ -94,8 +96,8 @@ if($resultado > 0){
         <td titulo='ACCIONES:'>
           <a class='link_editar' href='editar_usuario.php?DNI=$dni'>EDITAR</a>";
 
-            if($dni != $_SESSION['DNI']){
-              echo"
+          if ($dni != $_SESSION['DNI']) {
+            echo "
               <a class='link_eliminar' href='eliminar_usuario.php?DNI=$dni'>ELIMINAR</a>  
             ";
           }
@@ -103,49 +105,45 @@ if($resultado > 0){
         </td>
       </tr>
         ";
+        }
+      } else {
+        echo "<h3 style='text-align:-webkit-center'>No encontrado</h3>";
+      }
+      ?>
+      </table>
 
 
-    }
-  }
-  else 
-{
-echo "<h3 style='text-align:-webkit-center'>No encontrado</h3>";
-}
-?>
-</table>
-</div>
+    <div class="paginationUser">
+      <?php
+      if ($pagina > 1) {
+        echo "<li><a href='?pagina=" . ($pagina - 1) . "'>Anterior</a></li>";
+      }
 
-  
-<div class="paginationUser">
-<?php
-if ($pagina > 1) {
-  echo "<li><a href='?pagina=".($pagina-1)."'>Anterior</a></li>";
-}
+      for ($i = 1; $i <= $total_paginas; $i++) {
+        if ($i == $pagina) {
+          echo "<li><a class='pagina-actual'>$i</a></li>";
+        } else {
+          echo "<li><a href='?pagina=$i'>$i</a></li>";
+        }
+      }
 
-for ($i = 1; $i <= $total_paginas; $i++) {
-  if ($i == $pagina) {
-    echo "<li><a class='pagina-actual'>$i</a></li>";
-  } else {
-    echo "<li><a href='?pagina=$i'>$i</a></li>";
-  }
-}
-
-if ($pagina < $total_paginas) {
-  echo "<li><a href='?pagina=".($pagina+1)."'>Siguiente</a></li>";
-}
-?>
-</div>
+      if ($pagina < $total_paginas) {
+        echo "<li><a href='?pagina=" . ($pagina + 1) . "'>Siguiente</a></li>";
+      }
+      ?>
     </div>
 
-<?php
+    <?php
 
-mysqli_close($conexion); //cierra la BBDD
+    mysqli_close($conexion); //cierra la BBDD
 
-?>
+    ?>
 
 
 
-    </div>
+  </div>
+  </div>
 
 </body>
+
 </html>
