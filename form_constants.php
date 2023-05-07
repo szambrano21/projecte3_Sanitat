42c6ba472f3e8dc3dc9706f26fac1417928eac13
ruiznaomy13 <ruiznaomy13@gmail.com>
2023-05-08 00:00:50 +0200

    asignacion de id

--- form_constants.php
+++ form_constants.php
@@ -13,8 +13,10 @@
 
     h1,
     p {
-        margin: 0.5em auto;
+        margin: 0.5em auto 1em auto;
         text-align: center;
+        padding-bottom: 0.5em;
+        border-bottom: 1px solid #dadbee;
     }
 
     form {
@@ -37,7 +39,7 @@
         justify-content: space-between;
         align-content: space-around;
     }
- 
+
     label {
         margin: 0.5rem 0;
         width: 45%;
@@ -60,6 +62,10 @@
         padding: 0.5rem;
     }
 
+    input[type="radio"] {
+        height: 3em;
+    }
+
     .radio_input_section fieldset {
         display: inline-block;
         margin: 10px 15px 0 0;
@@ -69,7 +75,7 @@
 
     .inline {
         width: unset;
-        margin: 0 0.5em 0 0;
+        margin: 0 0 0 0.5em;
         vertical-align: middle;
     }
 
@@ -89,6 +95,11 @@
         margin: 0 15px 0 0;
     }
 
+    .radio_label {
+        width: 45%;
+        margin: 0.5em;
+    }
+
     .radio_input_section {
         margin: 2em;
     }
@@ -101,8 +112,13 @@
         resize: none;
         height: 15vh;
     }
+
+    .textarea_section label {
+        width: 100%;
+    }
+
     /* Estilos para dispositivos móviles */
-    @media only screen and (max-width: 850px) {
+    @media only screen and (max-width: 750px) {
         label {
             width: 80%;
         }
@@ -122,6 +138,20 @@
         .radio_input_section {
             width: 80%;
         }
+
+        .radio_label {
+            width: 80%;
+            display: flex;
+            margin: 0.5em;
+            flex-direction: row;
+            flex-wrap: wrap;
+            align-content: center;
+            justify-content: flex-start;
+        }
+
+        input[type="radio"] {
+            height: 0;
+        }
     }
 </style>
 <!DOCTYPE html>
@@ -140,40 +170,37 @@
         <h1>CONSTANTS</h1>
         <form method="post" action='https://register-demo.freecodecamp.org'>
             <fieldset>
-                <label for="first-name"><i class="fa-solid fa-weight-scale"></i> Temperatura (ºC): <input id="first-name" name="first-name" type="text" /></label>
-                <label for="last-name"><i class="fa-solid fa-stethoscope"></i> Pulsacions (ppm): <input id="last-name" name="last-name" type="text" /></label>
+                <label for="temperatura"><i class="fa-solid fa-weight-scale"></i> Temperatura (ºC): <input id="temperatura" name="temperatura" type="number" /></label>
+                <label for="pulsacions"><i class="fa-solid fa-stethoscope"></i> Pulsacions (ppm): <input id="pulsacions" name="pulsacions" type="number" /></label>
             </fieldset>
             <fieldset>
-                <label for="email"><i class="fa-sharp fa-solid fa-heart-pulse"></i> Pressió arterial (mm Hg):<input id="email" name="email" type="email" /></label>
-                <label for="new-password"><i class="fa-solid fa-eye-dropper"></i> Glucemia (mg/dL): <input id="new-password" name="new-password" type="password" pattern="[a-z0-5]{8,}" /></label>
+                <label for="presio_arterial"><i class="fa-sharp fa-solid fa-heart-pulse"></i> Pressió arterial (mm Hg):<input id="presio_arterial" name="presio_arterial" type="text" /></label>
+                <label for="glucemia"><i class="fa-solid fa-eye-dropper"></i> Glucemia (mg/dL): <input id="glucemia" name="glucemia" type="number" /></label>
             </fieldset>
             <fieldset>
-                <label for="email"><i class="fa-sharp fa-solid fa-gauge-high"></i> Saturació 02 (%):<input id="email" name="email" type="email" /></label>
-                <!-- <label for="new-password">Create a New Password: <input id="new-password" name="new-password" type="password" pattern="[a-z0-5]{8,}" /></label> -->
+                <label for="saturacio"><i class="fa-sharp fa-solid fa-gauge-high"></i> Saturació 02 (%):<input id="saturacio" name="saturacio" type="number" /></label>
             </fieldset>
             <fieldset class="radio_input_section">
                 <div>
                     EVN: <br>
-                    <label for="personal-account"><input id="personal-account" type="radio" name="account-type" class="inline" /> Si</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> No</label>
+                    <label for="evn"><input id="evn-1" type="radio" name="evn" class="inline" value="1" /> Si</label>
+                    <label for="evn"><input id="evn-0" type="radio" name="evn" class="inline" value="0"/> No</label>
                 </div>
                 <div>
                     Reavaluacio dolor: <br>
-                    <label for="personal-account"><input id="personal-account" type="radio" name="account-type" class="inline" /> Total</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> Parcial</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> Independent</label>
+                    <label for="reavaluacio-dolor"><input id="reavaluacio-dolor-total" type="radio" name="reavaluacio-dolor" class="inline" value="total"/> Total</label>
+                    <label for="business-account"><input id="reavaluacio-dolor-parcial" type="radio" name="reavaluacio-dolor" class="inline" value="parcial"/> Parcial</label>
+                    <label for="business-account"><input id="reavaluacio-dolor-independent" type="radio" name="reavaluacio-dolor" class="inline" value="independent"/> Independent</label>
                 </div>
                 <div>
                     Hemoglobina: <br>
-                    <label for="personal-account"><input id="personal-account" type="radio" name="account-type" class="inline" /> Si</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> No</label>
+                    <label for="hemoglobina"><input id="hemoglobina-1" type="radio" name="hemoglobina" class="inline" value="1"/> Si</label>
+                    <label for="hemoglobina"><input id="hemoglobina-0" type="radio" name="hemoglobina" class="inline" value="0"/> No</label>
                 </div>
-                <!-- </fieldset>
-            <fieldset> -->
             </fieldset>
             <fieldset>
-                <label for="Disfagia liquida"> Disfagia líquida: <textarea></textarea></label>
-                <label for="Disfagia sòlida"> Disfagia sòlida: <textarea></textarea></label>
+                <label for="disfagia-liquida"> Disfagia líquida: <textarea name="disfagia-liquida" id="disfagia-liquida"></textarea></label>
+                <label for="disfagia-solida"> Disfagia sòlida: <textarea name="disfagia-solida" id="disfagia-solida"></textarea></label>
             </fieldset>
             <input type="submit" value="Submit" />
         </form>
--- form_menjars.php
+++ form_menjars.php
@@ -170,39 +170,33 @@
         <h1>MENJAR I BEURE</h1>
         <form method="post" action='https://register-demo.freecodecamp.org'>
             <fieldset>
-                <label for="pes"><i class="fa-solid fa-weight-scale"></i> Pes (kg): <input id="first-name" name="pes" type="text" /></label>
-                <label for="talla"><i class="fa-solid fa-ruler-horizontal"></i> Mida (cm): <input id="last-name" name="pes" type="text" /></label>
+                <label for="pes"><i class="fa-solid fa-weight-scale"></i> Pes (kg): <input id="pes" name="pes" type="text" /></label>
+                <label for="mida"><i class="fa-solid fa-ruler-horizontal"></i> Mida (cm): <input id="mida" name="mida" type="text" /></label>
             </fieldset>
             <fieldset>
-                <label for="dieta_habitual"><i class="fa-solid fa-bowl-food"></i> Dieta habitual:<input id="email" name="email" type="email" /></label>
-                <label for="new-password"><i class="fa-solid fa-wheat-awn"></i> Aliments no grassos: <input id="new-password" name="new-password" type="password" pattern="[a-z0-5]{8,}" /></label>
+                <label for="dieta_habitual"><i class="fa-solid fa-bowl-food"></i> Dieta habitual:<input id="dieta_habitual" name="dieta_habitual" type="text" /></label>
+                <label for="aliments-no-grassos"><i class="fa-solid fa-wheat-awn"></i> Aliments no grassos: <input id="aliments-no-grassos" name="aliments-no-grassos" type="text" /></label>
             </fieldset>
             <fieldset>
-                <label for="email"><i class="fa-solid fa-wheat-awn-circle-exclamation"></i> Intolerancia:<input id="email" name="email" type="email" /></label>
+                <label for="intolerancia"><i class="fa-solid fa-wheat-awn-circle-exclamation"></i> Intolerancia:<input id="intolerancia" name="intolerancia" type="text" /></label>
             </fieldset>
             <fieldset class="radio_input_section">
                 <div><i class="fa-solid fa-handshake-angle"></i>
                     Necessitat d'ajut: <br>
-                    <label for="personal-account"><input id="personal-account" type="radio" name="account-type" class="inline" /> Total</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> Parcial</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> Independent</label>
+                    <label for="necessitat-ajut-total"><input id="necessitat-ajut-total" type="radio" name="necessitat-ajut" class="inline" value="total"/> Total</label>
+                    <label for="necessitat-ajut"><input id="necessitat-ajut-parcial" type="radio" name="necessitat-ajut" class="inline" value="parcial"/> Parcial</label>
+                    <label for="necessitat-ajut"><input id="necessitat-ajut-independent" type="radio" name="necessitat-ajut" class="inline" value="independent"/> Independent</label>
                 </div>
-                <div><i class="fa-sharp fa-solid fa-teeth-open"></i>
+                <div><i class="fa-sharp fa-solid fa-icicles"></i>
                     Inapetència i/o anorèxia: <br>
-                    <label for="personal-account"><input id="personal-account" type="radio" name="account-type" class="inline" /> Si</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> No</label>
+                    <label for="anorexia-1"><input id="anorexia-1" type="radio" name="anorexia" class="inline" value="1"/> Si</label>
+                    <label for="anorexia-0"><input id="anorexia-0" type="radio" name="anorexia" class="inline" value="0"/> No</label>
                 </div>
                 <div><i class="fa-sharp fa-solid fa-teeth-open"></i>
                     Protesis dental: <br>
-                    <label for="personal-account"><input id="personal-account" type="radio" name="account-type" class="inline" /> Si</label>
-                    <label for="business-account"><input id="business-account" type="radio" name="account-type" class="inline" /> No</label>
+                    <label for="protesis-dental-1"><input id="protesis-dental-1" type="radio" name="protesis-dental" class="inline" value="1"/> Si</label>
+                    <label for="protesis-dental-0"><input id="protesis-dental-0" type="radio" name="protesis-dental" class="inline" value="0"/> No</label>
                 </div>
-                <!-- </fieldset>
-            <fieldset> -->
-            </fieldset>
-            <fieldset>
-                <label for="Disfagia liquida"> Disfagia líquida: <textarea></textarea></label>
-                <label for="Disfagia sòlida"> Disfagia sòlida: <textarea></textarea></label>
             </fieldset>
             <input type="submit" value="Submit" />
         </form>
