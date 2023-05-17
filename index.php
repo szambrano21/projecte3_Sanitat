b96bfa564cf3f4b70b1c198eba1e5626511a9fd3
izambrano19 <izambrano19@ilg.cat>
2023-05-18 01:57:28 +0200

    avances

--- index.php
+++ index.php
@@ -3,111 +3,77 @@
 $alert = '';
 
 session_start();
-if(!empty($_SESSION['activo'])){
+if (!empty($_SESSION['activo'])) {
     header("location: inicial.php");
-}else{
+} else {
 
+    if (!empty($_POST)) {
+        if (empty($_POST["username"]) || empty($_POST["password"])) {
+            $alert = "INGRESA TU USUARIO Y CONTRASEÑA";
+        }else {
+            include_once('connexiobbddsanitat.php');
+            $user = mysqli_real_escape_string($conexion, $_POST["username"]);
+            // $pass = mysqli_real_escape_string($conexion, $_POST["password"]);
 
-if(!empty($_POST)){
-    if(empty($_POST["username"]) || empty($_POST["password"]) ) {
-        $alert = "INGRESA TU USUARIO Y CONTRASEÑA";
-    }else if($_POST["clau_temporal"]){
-
-        include_once('connexiobbddsanitat.php');
-
-        $clau = $_POST["clau_temporal"];
-
-        $query = mysqli_query($conexion, "SELECT * FROM tusuaris WHERE codi = '$clau' ");
-        $resultado = mysqli_num_rows($query);
-
-        if($resultado > 0){
-
-            while ($row = mysqli_fetch_assoc($query)) {
-
-                $codi = $row["codi"];
-      
-                if ($clau == $codi) {
-                    header("location: registrar_usuario_nuevo.php");
-
-                }
-
-              }
-        }else{
-            $alert = "LA CLAU ÉS INCORRECTA";
-        }
-
-    }
-    else{
-        include_once('connexiobbddsanitat.php');
-        $user = mysqli_real_escape_string($conexion, $_POST["username"]);
-        $pass = mysqli_real_escape_string($conexion, $_POST["password"]);
-
-/*        $pass = md5(mysqli_real_escape_string($conexion, $_POST["password"]));
-*/
-        $query = mysqli_query($conexion, "SELECT * FROM tusuaris WHERE nomUsuari = '$user' AND Password = '$pass' ");
-        $resultado = mysqli_num_rows($query);
-
-        if($resultado > 0){
-            $row = mysqli_fetch_assoc($query);
-            session_start();
-            $_SESSION['activo'] = true;
-            $_SESSION['DNI'] = $row['DNI'];
-            $_SESSION['nombre'] = $row['nomUsuari']; 
+            $pass = md5(mysqli_real_escape_string($conexion, $_POST["password"]));
             
-            $_SESSION['tipo'] = $row['tipo']; 
+            $query = mysqli_query($conexion, "SELECT * FROM tusuaris WHERE nomUsuari = '$user' AND Password = '$pass' ");
+            $resultado = mysqli_num_rows($query);
+
+            if ($resultado > 0) {
+                $row = mysqli_fetch_assoc($query);
+                session_start();
+                $_SESSION['activo'] = true;
+                $_SESSION['DNI'] = $row['DNI'];
+                $_SESSION['nombre'] = $row['nomUsuari'];
+
+                $_SESSION['tipo'] = $row['tipo'];
 
 
-            header("location: inicial.php");
-
-
-        }else{
-            $alert = "EL USUARIO Y LA CONTRASEÑA SON INCORRECTOS";
-            session_destroy();
+                header("location: inicial.php");
+            } else {
+                $alert = "EL USUARIO Y LA CONTRASEÑA SON INCORRECTOS";
+                session_destroy();
+            }
         }
     }
-}
 }
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    
-<?php include_once("scripts.php")?>
+
+    <?php include_once("scripts.php") ?>
+
 
 </head>
+
 <body>
 
-<div class="container_login">
-    <form class="login" action="index.php" id="formulario" method="post">
-        <div class="container_usuario">
-            <i class="fa-solid fa-user usuario_arriba"></i>
-        </div>
-        <label for="username">  
-            <i class="fa-solid fa-user"></i>
-            <input placeholder="username" type="text" name="username" id="username">
-            <span>admin</span>
-        </label>
-        <label>
-            <i class="fa-solid fa-lock"></i>
-            <input placeholder="password" type="password" name="password" id="password">
-        <span>admin</span>
-        </label>
-        <div class="alert"><?php echo isset($alert) ? $alert : ''; ?> </div>
-        
-        <label>
-            <p>clave temporal</p>
-            <input placeholder="clau_temporal" type="clau_temporal" name="clau_temporal" id="clau_temporal">
-        </label>
+    <div class="container_login">
+        <form class="login" action="index.php" id="formulario" method="post">
+            <div class="container_usuario">
+                <i class="fa-solid fa-user usuario_arriba"></i>
+            </div>
+            <label for="username">
+                <i class="fa-solid fa-user"></i>
+                <input placeholder="username" type="text" name="username" id="username">
+            </label>
+            <label>
+                <i class="fa-solid fa-lock"></i>
+                <input placeholder="password" type="password" name="password" id="password">
+            </label>
+            <div class="alert"><?php echo isset($alert) ? $alert : ''; ?> </div>
 
-        <br>
-        <input type="submit" value="LOGIN" class="button">
-    </form>
-</div>
+            <input type="submit" value="LOGIN" class="button" style="margin-bottom: 10px">
+            <a href="registrar_usuario_nuevo.php" class="button">REGISTRAR</a>
+
+        </form>
+    </div>
     <script src="login.js"></script>
 </body>
+
 </html>
-
-
-
--- registrar_usuario_nuevo.php
+++ registrar_usuario_nuevo.php
@@ -2,35 +2,54 @@
 
 include_once('connexiobbddsanitat.php');
 
-if(!empty($_POST)){
-    $alert="";
-    if(empty($_POST["nombre"]) || empty($_POST["dni"]) || empty($_POST["password"]) || empty($_POST["tipo"])){
+if (!empty($_POST)) {
+    $alert = "";
+    if (empty($_POST["nombre"]) || empty($_POST["dni"]) || empty($_POST["password"]) || empty($_POST["tipo"]) || empty($_POST["clau_temporal"])) {
 
-        $alert="<p class='msg_error'>Todos los campos son obligatorios</p>";
-    }else{
-
+        $alert = "<p class='msg_error'>Todos los campos son obligatorios</p>";
+    } else {
         $nombre = $_POST["nombre"];
         $dni = $_POST["dni"];
         $cognom = $_POST["cognomUsuari"];
         $telefono = $_POST["telefono"];
-        $pass = md5($_POST["password"]);
+        $pass = md5(mysqli_real_escape_string($conexion, $_POST["password"]));
+
         $tipo = $_POST["tipo"];
+        $clau_temporal = $_POST["clau_temporal"];
 
 
-        $query = mysqli_query($conexion,"SELECT * FROM tusuaris WHERE DNI = '$dni'");
+        $query = mysqli_query($conexion, "SELECT * FROM tusuaris WHERE DNI = '$dni'");
         $resultado = mysqli_fetch_assoc($query);
 
-        if($resultado > 0){
-            $alert="<p class='msg_error'>El usuario ya existe</p>";
-        }else{
+        if ($resultado > 0) {
+            $alert = "<p class='msg_error'>El usuario ya existe</p>";
+        } else {
 
-            $query_insertar = mysqli_query($conexion, "INSERT INTO tusuaris (DNI, tipo, nomUsuari, cognomUsuari, telefono, Password)
-            VALUES('$dni','$tipo','$nombre',' $cognom ',' $telefono ','$pass')");
+            $query = mysqli_query($conexion, "SELECT * FROM tusuaris WHERE codi = '$clau_temporal' ");
+            $resultado = mysqli_num_rows($query);
 
-            if($query_insertar){
-                $alert="<p class='msg_correcto'>L'usuari ha sigut creat correctament</p>";
-            }else{
-                $alert="<p class='msg_error'>Error al crear el usuario</p>";
+            if ($resultado > 0) {
+
+                while ($row = mysqli_fetch_assoc($query)) {
+
+                    $codi = $row["codi"];
+
+                    if ($clau_temporal == $codi) {
+
+                        $query_insertar = mysqli_query($conexion, "INSERT INTO tusuaris (DNI, tipo, nomUsuari, cognomUsuari, telefono, Password)
+                        VALUES('$dni','$tipo','$nombre',' $cognom ',' $telefono ','$pass')");
+
+                        if ($query_insertar) {
+                            $alert = "<p class='msg_correcto'>L'usuari ha sigut creat correctament</p>";
+                        } else {
+                            $alert = "<p class='msg_error'>Error al crear el usuario</p>";
+                        }
+                    } else {
+                        $alert = "<p class='msg_error'>La clau es incorrecta</p>";
+                    }
+                }
+            } else {
+                $alert = "<p class='msg_error'>Error al crear el usuario</p>";
             }
         }
     }
@@ -40,68 +59,108 @@ if(!empty($_POST)){
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-<?php include_once("scripts.php"); ?>
+    <?php include_once("scripts.php"); ?>
 
+    <script>
+        window.onload = function() {
 
+            let input_clau = document.getElementById('clau_temporal');
+            input_clau.value = "";
 
-<script src="js/validate_user.js"></script>
-<link rel="stylesheet" href="css/validate.css">
+        }
+    </script>
+
+    <style>
+        .container_login {
+            flex-direction: column;
+        }
+
+        .login label {
+            justify-content: space-between;
+            margin-bottom: 25px;
+        }
+
+        .container_login .submitRegister , .container_login a{
+
+            color: #fff;
+            border: none;
+            background-color: #1F354A;
+            border-radius: 25px;
+            padding: 10px 15px;
+            cursor: pointer;
+            font-size: 20px;
+            display: flex;
+            width: 100%;
+            text-decoration: none;
+            max-width: 300px;
+            align-items: center;
+            justify-content: center;
+            margin: auto;
+        }
+
+        input,
+        select {
+            min-height: 30px;
+            font-size: 16px;
+        }
+    </style>
 
 </head>
+
 <body>
-<?php 
+    <?php
 
-session_start();
+    session_start();
 
-?>
-
-
-
-    
+    ?>
 
 
-    <div class="container_general">
-            <div class="second_container">
-                <div class="form_dades_container">
-        <h1>CREAR USUARIO</h1>
+
+
+
+
+
+    <div class="container_login">
+        <h1>REGISTRO USUARIO</h1>
+
         <div class="alert"> <?php echo isset($alert) ? $alert : ''; ?> </div>
         <hr>
-        <form action="" id="validate" method="post">
-            <fieldset>
-                <label for="nombre">Nom
-                    <input type="text" name="nombre" id="nombre" placeholder="Nombre">
-                </label>
-            
-                <label for="cognomUsuari">Cognom Usuari
-                    <input type="text" name="cognomUsuari" id="cognomUsuari" placeholder="cognomUsuari" >
-                </label>
-            </fieldset>
-            <fieldset>
-                <label for="dni">DNI
+        <form class="login" action="#" id="registro" method="post" style="max-width: 450px">
+            <label for="nombre">Nom
+                <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+            </label>
+
+            <label for="cognomUsuari">Cognom Usuari
+                <input type="text" name="cognomUsuari" id="cognomUsuari" placeholder="cognomUsuari">
+            </label>
+            <label for="dni">DNI
                 <input type="text" name="dni" id="dni" placeholder="DNI" minlength="9" maxlength="9">
-                </label>
-                <label for="telefono">telefono
-                    <input type="text" name="telefono" id="telefono" placeholder="telefono" minlength="9" maxlength="9">
-                </label>
-            </fieldset>
-            <fieldset>
-                <label for="password">Contrasenya
-                    <input type="password" name="password" id="password" placeholder="Contrasenya">
-                </label>    
-                <label for="tipo">Tipus Usuari:
-                    <select name="tipo" id="tipo">
-                        <option value="admin">admin</option>
-                        <option value="usuario">usuari</option>
-                    </select>
-                </label>
-            </fieldset>
-            
-            <input type="submit" value="Crear Usuari" class="submitForm">
+            </label>
+            <label for="telefono">telefono
+                <input type="text" name="telefono" id="telefono" placeholder="telefono" minlength="9" maxlength="9">
+            </label>
+            <label for="password">Contrasenya
+                <input type="password" name="password" id="password" placeholder="Contrasenya">
+            </label>
+            <label for="tipo">Tipus Usuari:
+                <select name="tipo" id="tipo">
+                    <option value="usuario">usuari</option>
+                </select>
+            </label>
+
+            <label for="clau_temporal">Clau temporal
+                <input type="password" name="clau_temporal" id="clau_temporal" placeholder="clau_temporal">
+            </label>
+
+            <input type="submit" value="REGISTRAR" class="submitRegister" style="margin-bottom: 10px">
+            <a href="index.php" class="button">TORNAR</a>
+
 
         </form>
 
     </div>
-</div>
 </body>
+
 </html>
